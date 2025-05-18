63ee446318814fad825b0fc395044b51e0204d0e
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-05-18 23:21:15 +0900

    init: spring 프로젝트 초기화

---
+++ src/main/java/com/valanse/valanse/ValanseApplication.java
@@ -0,0 +1,13 @@
+package com.valanse.valanse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValanseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValanseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valanse/valanse/ValanseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valanse.valanse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValanseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
