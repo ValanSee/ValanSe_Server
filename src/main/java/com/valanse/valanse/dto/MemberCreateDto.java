5054583904c71ffd41fe1d5fd27acf2e13a69430
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-05-27 02:14:54 +0900

    feat: jwt 로그인 기능 구현

---
+++ src/main/java/com/valanse/valanse/dto/MemberCreateDto.java
@@ -0,0 +1,13 @@
+package com.valanse.valanse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberCreateDto {
+    private String email;
+    private String password;
+}
