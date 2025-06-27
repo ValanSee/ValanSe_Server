082cae73e6e76289272690aa76d4798a6d2a1696
YoungseoChoi8482 <cys03380338@gmail.com>
2025-06-28 02:23:06 +0900

    feat:핫이슈 투표 선택지 취소 및 투표 구현

--- src/main/java/com/valanse/valanse/controller/AuthController.java
+++ src/main/java/com/valanse/valanse/controller/AuthController.java
@@ -18,10 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -127,4 +124,5 @@ public class AuthController {
         memberService.deleteMemberById(); // 사용자 데이터베이스 내용 삭제
         return ResponseEntity.ok().build();
     }
+
 }
