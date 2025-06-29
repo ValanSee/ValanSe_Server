7b1645ffb9909ed2ade9bb16ead53afdb371536d
김민지 <minzikx@sookmyung.ac.kr>
2025-06-29 19:18:33 +0900

    fix: 댓글 작성 시 createdAt 필드 null 로 저장되는 이슈 수정

--- src/main/java/com/valanse/valanse/ValanseApplication.java
+++ src/main/java/com/valanse/valanse/ValanseApplication.java
@@ -2,8 +2,10 @@ package com.valanse.valanse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ValanseApplication {
 
 	public static void main(String[] args) {
