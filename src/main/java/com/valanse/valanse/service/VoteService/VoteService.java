0b41ef9bcdde6bb27cc9e2586e8bb97ad21023cd
mnjese <mjk5949@naver.com>
2025-07-01 11:23:34 +0900

    [fix] 멤버 참조 에러 2차 수정

--- src/main/java/com/valanse/valanse/service/VoteService/VoteService.java
+++ src/main/java/com/valanse/valanse/service/VoteService/VoteService.java
@@ -1,10 +1,13 @@
 package com.valanse.valanse.service.VoteService;
 
+import com.valanse.valanse.common.api.ApiException;
 import com.valanse.valanse.domain.Member;
 import com.valanse.valanse.domain.Vote;
 import com.valanse.valanse.dto.Vote.VoteResponseDto;
+import com.valanse.valanse.repository.MemberRepository;
 import com.valanse.valanse.repository.VoteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +18,12 @@ import java.util.stream.Collectors;
 public class VoteService {
 
     private final VoteRepository voteRepository;
+    private final MemberRepository memberRepository;
 
-    public List<VoteResponseDto> getMyCreatedVotes(Member member, String sort) {
+    public List<VoteResponseDto> getMyCreatedVotes(Long memberId, String sort) {
+        Member member = memberRepository.findByIdAndDeletedAtIsNull(memberId)
+                .orElseThrow(() -> new ApiException("회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+
         List<Vote> votes = sort.equals("latest") ?
                 voteRepository.findAllByMemberOrderByCreatedAtDesc(member) :
                 voteRepository.findAllByMemberOrderByCreatedAtAsc(member);
@@ -24,7 +31,10 @@ public class VoteService {
         return votes.stream().map(VoteResponseDto::new).collect(Collectors.toList());
     }
 
-    public List<VoteResponseDto> getMyVotedVotes(Member member, String sort) {
+    public List<VoteResponseDto> getMyVotedVotes(Long memberId, String sort) {
+        Member member = memberRepository.findByIdAndDeletedAtIsNull(memberId)
+                .orElseThrow(() -> new ApiException("회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND));
+
         List<Vote> votes = sort.equals("latest") ?
                 voteRepository.findAllByMemberVotedOrderByCreatedAtDesc(member) :
                 voteRepository.findAllByMemberVotedOrderByCreatedAtAsc(member);
