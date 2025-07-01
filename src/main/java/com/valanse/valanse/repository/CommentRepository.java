ea08fe345b22d05988ff2d34a272b5ee12910283
mnjese <mjk5949@naver.com>
2025-07-01 10:31:18 +0900

    [fix] 멤버 참조 에러 수정

--- src/main/java/com/valanse/valanse/repository/CommentRepository.java
+++ src/main/java/com/valanse/valanse/repository/CommentRepository.java
@@ -3,8 +3,11 @@ package com.valanse.valanse.repository;
 import com.valanse.valanse.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByCommentGroupIdAndIsDeletedFalseAndParentIsNull(Long groupId);
+    List<Comment> findByCommentGroupIdAndIsDeletedFalse(Long groupId);
     // 필요한 경우 사용자 정의 쿼리 메서드 추가 가능
 }
