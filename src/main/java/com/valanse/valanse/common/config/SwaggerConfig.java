1ce3ed0dee052a97514e462d1852e6b971fc8f2a
김민지 <minzikx@sookmyung.ac.kr>
2025-07-02 01:25:52 +0900

    feat: push branch 및 설정 수정

--- src/main/java/com/valanse/valanse/common/config/SwaggerConfig.java
+++ src/main/java/com/valanse/valanse/common/config/SwaggerConfig.java
@@ -29,10 +29,7 @@ public class SwaggerConfig {
                         .version("v1") // API 버전
                         .description("OAuth 로그인은 <strong>프론트엔드에서 먼저 진행</strong>해 주세요.<br><br>" +
                                 "프론트에서 로그인 성공 시 발급받은 <strong>access token</strong>을 Swagger의 <strong>Authorize 버튼</strong>에 입력해주시면 됩니다!<br>" +
-                                "이때, <strong>Bearer 없이</strong> 토큰 값만 입력해 주세요.<br><br>" +
-                                "⚠️ 주의: Swagger에서는 로그인 처리를 직접 수행할 수 없습니다. <br>" +
-                                "반드시 프론트엔드 배포 URL (현재 https://valan-se-web.vercel.app로 설정됨) 에서 로그인 요청을 통해 code 발급 후, <br>" +
-                                "POST /auth/kakao/login 로 요청을 보내셔서 access token을 발급받는 것까지 진행하신 후에 swagger에서 테스트해주셔야 합니다." )
+                                "이때, <strong>Bearer 없이</strong> 토큰 값만 입력해 주세요.<br><br>" )
                 )
                 // 전역 보안 설정: 모든 API 호출 시 JWT 토큰 필요하도록 설정
                 .addSecurityItem(new SecurityRequirement().addList(jwtSchemeName))
