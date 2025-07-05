1557d95099fa54ccd5a61e52abeb3132407c69f4
김민지 <minzikx@sookmyung.ac.kr>
2025-07-06 00:01:04 +0900

    fix: 어노테이션 누락 수정

--- src/main/java/com/valanse/valanse/repository/CommentRepository.java
+++ src/main/java/com/valanse/valanse/repository/CommentRepository.java
@@ -4,9 +4,11 @@ import com.valanse.valanse.domain.Comment;
 import com.valanse.valanse.repository.CommentRepositoryCustom.CommentRepositoryCustom;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
     @EntityGraph(attributePaths = {"member", "member.memberVoteOptions", "member.memberVoteOptions.voteOption"})
     List<Comment> findAllByParentId(Long parentId);
