079c9edbc124662875ad430de7547011e515a586
SyaDo95 <70837945+SyaDo95@users.noreply.github.com>
2025-07-06 22:33:50 +0900

    feat votecheck fin

--- src/main/java/com/valanse/valanse/service/VotesCheckService/VotesCheckServiceImpl.java
+++ src/main/java/com/valanse/valanse/service/VotesCheckService/VotesCheckServiceImpl.java
@@ -1,9 +1,18 @@
 package com.valanse.valanse.service.VotesCheckService;
 
+import com.valanse.valanse.domain.MemberProfile;
+import com.valanse.valanse.domain.mapping.MemberVoteOption;
+import com.valanse.valanse.dto.VotesCheck.VoteAgeResultResponseDto;
 import com.valanse.valanse.dto.VotesCheck.VoteGenderResultResponseDto;
+import com.valanse.valanse.dto.VotesCheck.VoteMbtiResultResponseDto;
 import com.valanse.valanse.repository.VotesCheckRepositoryCustom.VoteResultQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,4 +24,16 @@ public class VotesCheckServiceImpl implements VotesCheckService {
     public VoteGenderResultResponseDto getGenderVoteResult(Long voteId, String gender) {
         return voteResultQueryRepository.findVoteResultByGender(voteId, gender);
     }
+
+    @Override
+    public VoteAgeResultResponseDto getAgeVoteResult(Long voteId) {
+        return voteResultQueryRepository.findVoteResultByAge(voteId);
+    }
+
+    @Override
+    public VoteMbtiResultResponseDto getVoteResultByMbti(Long voteId, String mbtiType) {
+        return voteResultQueryRepository.findVoteResultByMbti(voteId, mbtiType);
+    }
+
+
 }
