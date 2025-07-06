4242583866bfc2e5df11feb2400ab3efce88c4a9
김민지 <minzikx@sookmyung.ac.kr>
2025-07-07 00:56:55 +0900

    fix: 배포 cicd 테스트용 커밋

--- src/main/java/com/valanse/valanse/common/config/SwaggerConfig.java
+++ src/main/java/com/valanse/valanse/common/config/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Valanse API Docs")
                         .version("v1") // API 버전
-                        .description("OAuth 로그인은 <strong>프론트엔드에서 먼저 진행</strong>해 주세요.<br><br>" +
+                        .description("카카오 OAuth 로그인은 <strong>프론트엔드에서 먼저 진행</strong>해 주세요.<br><br>" +
                                 "프론트에서 로그인 성공 시 발급받은 <strong>access token</strong>을 Swagger의 <strong>Authorize 버튼</strong>에 입력해주시면 됩니다!<br>" +
                                 "이때, <strong>Bearer 없이</strong> 토큰 값만 입력해 주세요.<br><br>" )
                 )
