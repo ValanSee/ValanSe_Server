513eabc743c328bbd92eeed18a86134cd5383770
mnjese <mjk5949@naver.com>
2025-07-21 12:48:54 +0900

    feat: 내가 쓴 댓글에 닉네임 및 투표 제목 추가

--- src/main/java/com/valanse/valanse/dto/Comment/MyCommentResponseDto.java
+++ src/main/java/com/valanse/valanse/dto/Comment/MyCommentResponseDto.java
@@ -13,22 +13,34 @@ import java.time.LocalDateTime;
 public class MyCommentResponseDto {
 
     private Long id;
-    private String title; // 추가
+    private String title;
     private String content;
     private Long memberId;
     private String memberName;
     private boolean isReply;
     private LocalDateTime createdAt;
+    private Long voteOwnerId;
+    private String voteOwnerName;     // 추가
+    private String voteTitle;         // 추가
+    private String voteOptionLabel;   // 추가
 
     public static MyCommentResponseDto fromEntity(Comment comment) {
         return MyCommentResponseDto.builder()
                 .id(comment.getId())
-                .title(comment.getTitle())    //  추가
+                .title(comment.getTitle())
                 .content(comment.getContent())
                 .memberId(comment.getMember().getId())
                 .memberName(comment.getMember().getName())
                 .isReply(comment.getParent() != null)
                 .createdAt(comment.getCreatedAt())
+                .voteOwnerId(comment.getCommentGroup().getVote().getMember().getId())
+                .voteOwnerName(comment.getCommentGroup().getVote().getMember().getName())   // 추가
+                .voteTitle(comment.getCommentGroup().getVote().getTitle())                  // 추가
+                .voteOptionLabel(comment.getCommentGroup().getVote().getVoteOptions().stream()
+                        .filter(vo -> vo.getId().equals(comment.getId()))                  //  comment에 연결된 voteOption id 매칭 필요
+                        .map(vo -> vo.getLabel().name())
+                        .findFirst()
+                        .orElse(null))                                                     // 추가
                 .build();
     }
 }
