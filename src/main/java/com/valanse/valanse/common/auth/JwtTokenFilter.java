105b38d8e9d573a52b0ec260e618dfe80d34716c
김민지 <minzikx@sookmyung.ac.kr>
2025-07-24 17:43:52 +0900

    Docs: cicd 트리거용 커밋

--- src/main/java/com/valanse/valanse/common/auth/JwtTokenFilter.java
+++ src/main/java/com/valanse/valanse/common/auth/JwtTokenFilter.java
@@ -48,7 +48,7 @@ public class JwtTokenFilter extends GenericFilter {
                         uri.startsWith("/webjars") ||
                         uri.equals("/votes/best") ||
 
-                        // 추가된 부분: GET /votes (목록 조회) 경로 허용
+                        // GET /votes (목록 조회) 경로 허용
                         (uri.equals("/votes") && request.getMethod().equals("GET")) ||
 
                         // 추가된 부분: GET /votes/{voteId} (상세 조회) 경로 허용
