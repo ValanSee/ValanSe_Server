36c2c0c224f89df3ac0f24936cf7d18489c4bbe9
5solbin <dhrhfqls@naver.com>
2026-05-28 14:43:35 +0900

    fix : 카테고리 3종류 추가

--- src/main/java/com/valanse/valanse/domain/enums/VoteCategory.java
+++ src/main/java/com/valanse/valanse/domain/enums/VoteCategory.java
@@ -1,5 +1,5 @@
 package com.valanse.valanse.domain.enums;
 
 public enum VoteCategory {
-    ALL, FOOD, LOVE, ETC // 연애, 음식, 기타
+    ALL, FOOD, LOVE, BUY, SPORT, WORRY, ETC // 연애, 음식, 살까말까, 스포츠, 고민상담, 기타
 }
